74cb9efeedb7965464d477cef2765b0fe53cb9b9
kykt35 <kykt35@gmail.com>
2019-04-28 15:34:46 +0900

    Add axios-module

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,14 @@ module.exports = {
   ** Customize the progress bar color
   */
   loading: { color: '#3B8070' },
-  /*
+  modules: 
+    [
+      '@nuxtjs/axios'
+    ],
+    axios: {
+
+    },
+    /*
   ** Build configuration
   */
   build: {
